2b34b0e75bcf27608756abcefa4062c287b02a69
mattia laurenzana <mattia.laurenzana@beije.it>
2022-11-22 09:50:22 +0100

    Set button component

---
+++ src/components/crossButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from "react-native";
+
+
+function crossButton() {
+  return (
+    <Button 
+      title={"Button"}
+    />
+  )
+}
+
+export default crossButton

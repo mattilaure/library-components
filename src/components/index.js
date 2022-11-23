072252d2d30998dbf3e6ca1c05e929a6882aefe3
Daniele Zappalà <daniele.zappala@beije.it>
2022-11-23 12:11:44 +0100

    export game

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import CrossButton from "./button/CrossButton";
 import CrossInputBox from "./inputBox/CrossInputBox";
 import CrossText from "./text/CrossText";
+import Game from "./game/Game";
 
 export {
     CrossButton,
     CrossText,
-    CrossInputBox
+    CrossInputBox,
+    Game
 }

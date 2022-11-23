0f4426f44723b1d96c26c220163ab71912e4f1d5
Daniele Zappalà <daniele.zappala@beije.it>
2022-11-23 09:40:48 +0100

    update version

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import CrossButton from "./button/CrossButton";
 import CrossInputBox from "./inputBox/CrossInputBox";
 import CrossText from "./text/CrossText";
-import Home from "./home/Home";
 
 export {
     CrossButton,
     CrossText,
-    CrossInputBox,
-    Home
+    CrossInputBox
 }
